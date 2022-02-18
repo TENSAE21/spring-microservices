46fed31a78b987a230b1b716c3a2f4d265a3d8d6
Tensae <tensaeberhan@ymail.com>
2022-02-18 15:53:34 -0600

    Rest Template communication between services

--- customer/src/main/java/com/tensae/customer/CustomerService.java
+++ customer/src/main/java/com/tensae/customer/CustomerService.java
@@ -1,9 +1,10 @@
 package com.tensae.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -12,7 +13,22 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .email(request.email())
                 .build();
 
-        customerRepository.save(customer);
+        //So the customer will have a DB ID
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+                );
+
+        if(fraudCheckResponse.isFraudlentCustomer())
+        {
+            throw new IllegalStateException("Fradulent Customer");
+        }
+
+
+
+
 
     }
 }
---
+++ customer/src/main/java/com/tensae/customer/FraudCheckResponse.java
@@ -0,0 +1,5 @@
+package com.tensae.customer;
+
+public record FraudCheckResponse(Boolean isFraudlentCustomer){
+
+}
